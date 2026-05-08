e813a4d709ebce5932689019a5845a671e83e8fd
liuliu.mz <71892967@qq.com>
2026-05-08 12:30:00 +0800

    修正IDE插件运行报错

--- MzAgentIDEPackage.pas
+++ MzAgentIDEPackage.pas
@@ -7,13 +7,56 @@ procedure Register;
 implementation
 
 uses
-  ToolsAPI, MzAgentIDEWizard, MzAgentIDEDockForm;
+  Winapi.Windows, System.SysUtils, ToolsAPI, MzAgentIDEWizard, MzAgentIDEDockForm;
+
+var
+  FDockableForm: TMzAgentIDEDockForm;
+  FWizardIndex: Integer = -1;
 
 procedure Register;
+var
+  Services: INTAServices;
+  WizardServices: IOTAWizardServices;
 begin
-  (BorlandIDEServices as INTAServices).RegisterDockableForm(
-    TMzAgentIDEDockForm.Create);
-  RegisterPackageWizard(TMzAgentIDEWizard.Create);
+  OutputDebugString('Register: enter');
+
+  if not Assigned(BorlandIDEServices) then
+  begin
+    OutputDebugString('Register: BorlandIDEServices nil, exit');
+    Exit;
+  end;
+  OutputDebugString('Register: BorlandIDEServices OK');
+
+  Services := BorlandIDEServices as INTAServices;
+  WizardServices := BorlandIDEServices as IOTAWizardServices;
+  OutputDebugString('Register: Services obtained');
+
+  if Assigned(FDockableForm) then
+  begin
+    OutputDebugString('Register: UnregisterDockableForm...');
+    Services.UnregisterDockableForm(FDockableForm);
+    OutputDebugString('Register: UnregisterDockableForm OK');
+    FDockableForm := nil;
+  end;
+
+  FDockableForm := TMzAgentIDEDockForm.Create;
+  OutputDebugString('Register: RegisterDockableForm...');
+  Services.RegisterDockableForm(FDockableForm);
+  OutputDebugString('Register: RegisterDockableForm OK');
+
+  if FWizardIndex >= 0 then
+  begin
+    OutputDebugString('Register: RemoveWizard...');
+    WizardServices.RemoveWizard(FWizardIndex);
+    OutputDebugString('Register: RemoveWizard OK');
+    FWizardIndex := -1;
+  end;
+
+  OutputDebugString('Register: AddWizard...');
+  FWizardIndex := WizardServices.AddWizard(TMzAgentIDEWizard.Create);
+  OutputDebugString(PChar('Register: AddWizard OK, index=' + IntToStr(FWizardIndex)));
+
+  OutputDebugString('Register: exit');
 end;
 
 end.
