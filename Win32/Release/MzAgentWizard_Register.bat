aad78f4ee4cdc02b9b6bcf003bc090b2e4cb0a52
liuliu.mz <71892967@qq.com>
2026-05-12 18:18:00 +0800

    增加IDE插件注册批处理

---
+++ Win32/Release/MzAgentWizard_Register.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+reg add "HKCU\Software\Embarcadero\BDS\23.0\Experts" /v MzAgent /t REG_SZ /d "%~dp0MzAgentWizard.dll" /f
+pause
---
+++ Win32/Release/MzAgentWizard_Unregister.bat
@@ -0,0 +1,3 @@
+﻿@echo off
+reg delete "HKCU\Software\Embarcadero\BDS\23.0\Experts" /v MzAgent /f
+pause
